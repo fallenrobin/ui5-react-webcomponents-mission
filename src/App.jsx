c67bb392542e71868f89f78fbc976311e218cd79
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-07-18 16:39:11 -0500

    'MyApp renders'

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MyApp } from './MyApp';
+import {ThemeProvider} from "@ui5/webcomponents-react";
+
+
+
+function App() {
+  return (
+
+    <ThemeProvider>
+
+      <MyApp />
+
+    </ThemeProvider>
+
+  );
+}
+
+export default App;
