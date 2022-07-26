981b0475af436423738173ab724d8fa5d7a800bf
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-07-26 14:54:00 -0500

    add UI5 charts

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,19 +1,58 @@
 import {
     Card,
     Text,
-    CardHeader,
-
+    CardHeader
 }
     from "@ui5/webcomponents-react";
 import { spacing }
     from "@ui5/webcomponents-react-base";
-import React from "react";
+import {
+    BarChart,
+    LineChart
+}
+    from "@ui5/webcomponents-react-charts";
+
+import React, { useState } from "react";
+
+const dataset = [
+    {
+        month: "January",
+        data: 65
+    },
+    {
+        month: "February",
+        data: 59
+    },
+    {
+        month: "March",
+        data: 80
+    },
+    {
+        month: "April",
+        data: 81
+    },
+    {
+        month: "May",
+        data: 56
+    },
+    {
+        month: "June",
+        data: 55
+    },
+    {
+        month: "July",
+        data: 40
+    }
+];
+
 
 function MyApp() {
 
-const handleHeaderClick = () => {
-    alert("Header clicked!")
-};
+    const [toggleCharts, setToggleCharts] = useState("lineChart");
+
+    const handleHeaderClick = () => {
+        alert("Header clicked!")
+    };
 
     return (
 
@@ -23,10 +62,23 @@ const handleHeaderClick = () => {
                 interactive
                 onClick={handleHeaderClick}
             />
-            }>
+        }
+            style={{ width: "300px" }}
+        >
             <Text style={spacing.sapUiContentPadding}>
                 This is the content area of the Card
             </Text>
+            <LineChart
+                measures={[{ accessor: "month" }]}
+                dimensions={[{ accessor: "data", label: "Stock Price" }]}
+                dataset={dataset}
+            />
+            <BarChart
+                dimensions={[{ accessor: "month" }]}
+                measures={[{ accessor: "data", label: "Stock Price" }]}
+                dataset={dataset}
+            />
+
         </Card>
 
     )
