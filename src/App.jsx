57874a0e7734e2f22a0370d275180607fbc29a9d
Juliette Lelchuk <juliettelelchuk@mindsetconsulting.com>
2022-07-27 15:48:55 -0500

    add Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { ThemeProvider } from "@ui5/webcomponents-react";
 import MyApp from './MyApp';
 // import { MyApp } from './MyApp'; via tutorial; wrong way to import
+import { HashRouter } from "react-router-dom";
 
 
 function App() {
   return (
+    <HashRouter>
+      <ThemeProvider>
 
-    <ThemeProvider>
+        <MyApp />
 
-      <MyApp />
-
-    </ThemeProvider>
-
+      </ThemeProvider>
+    </HashRouter>
   );
 }
 
---
+++ src/Detail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title } from "@ui5/webcomponents-react";
+
+
+function Detail() {
+  return (
+    <Title>Hello world!</Title>
+  )
+}
+
+export default Detail
